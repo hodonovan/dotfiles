7ca1681d45dee5b8103ea63b8ab4099963657f60
Donovan Ho <donovanduyho@gmail.com>
2026-06-06 20:57:37 -0400

    Add btop config

--- home/terminal/shell/default.nix
+++ home/terminal/shell/default.nix
@@ -32,19 +32,76 @@
       };
     };
  
-    zoxide = { enable = true; };
-
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+    };
+
+    zoxide = { enable = true; };
+    
+    btop = {
+      enable = true;
+
+      settings = {
+        color_theme = "greyscale";
+        theme_background = false;
+      };
+    };
+
+    fastfetch = {
+      enable = true;
+
+      settings = {
+        logo = {
+          padding = {
+            top = 1;
+            left = 3;
+            right = 3;
+          };
+          color = {
+            "1" = "38;2;176;176;176";
+            "2" = "38;2;71;71;71";
+          };
+        };
+        
+        display = {
+          color = {
+            keys = "38;2;255;255;255";
+            title = "38;2;255;255;255";
+          };
+        };
+
+        modules = [
+          "title"
+          "separator"
+          {
+            type = "host";
+            format = "{1}";
+          }
+          {
+            type = "os";
+            format = "{3}";
+          }
+          "uptime"
+          "shell"
+          {
+            type = "terminal";
+            format = "{5}"; 
+          }
+          "cpu"
+          "memory"
+          {
+            type = "disk";
+            format = "{1} / {2} ({3})";
+          }
+        ];
+      };
     };
   };
 
   home.packages = with pkgs; [
     bat
-    btop
     curl
-    fastfetch
     fd
     ripgrep
     tree
