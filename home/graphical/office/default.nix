6bdbe2a4539e9639d777d4b264810a424036c3d5
Donovan Ho <donovanduyho@gmail.com>
2026-06-06 23:19:08 -0400

    Add zathura

--- home/graphical/office/default.nix
+++ home/graphical/office/default.nix
@@ -1,6 +1,32 @@
 { pkgs, ... }:
 
 {
+  programs.zathura = {
+    enable = true;
+
+    mappings = {
+      "f" = "toggle_fullscreen";
+      "[fullscreen] f" = "toggle_fullscreen";
+    };
+
+    options = {
+      font = "JetBrainsMono Nerd Font 9";
+      guioptions = "";
+      adjust-open = "width";
+      window-title-basename = "true";
+      default-bg = "#101010";            
+      default-fg = "#b0b0b0";            
+      statusbar-bg = "#272727";          
+      statusbar-fg = "#b0b0b0";          
+      inputbar-bg = "#101010";           
+      inputbar-fg = "#ffffff";           
+      completion-bg = "#272727";         
+      completion-fg = "#b0b0b0";         
+      completion-highlight-bg = "#474747"; 
+      completion-highlight-fg = "#ffffff";
+    };
+  };
+
   home.packages = with pkgs; [
     obsidian
     zotero
